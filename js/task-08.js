76c1418cc516d99489ca58c3099d597db9cf8195
stryzhko <stryzhko@gmail.com>
2022-11-06 01:52:21 +0200

    add task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector(".login-form");
+
+const handleBtnLogin = (e) => {
+    e.preventDefault();
+    
+  const {
+    elements: { email, password },
+  } = e.currentTarget;
+    
+  if (email.value.trim() === "" || password.value.trim() === "") {
+    return alert("Fill in all fields!");
+  }
+    
+    console.log({ Login: email.value, Password: password.value });
+    
+  e.currentTarget.reset();
+}
+
+formEl.addEventListener("submit", handleBtnLogin);
 
