058bfdfd426466ad4088013d4539554554987ad9
stryzhko <stryzhko@gmail.com>
2022-11-06 13:35:55 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const colorCurrentSpanEl = document.querySelector(".color");
+
+const changeColorBtnClick = () => {
+  const colorRandom = getRandomHexColor();
+  document.body.style.backgroundColor = colorRandom;
+  colorCurrentSpanEl.textContent = colorRandom;
+}
+
+changeColorBtn.addEventListener("click", changeColorBtnClick);
